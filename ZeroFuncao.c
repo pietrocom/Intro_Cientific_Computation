5d8d27b86824822b735f1fc5a032418c3404947c
pietrocom <pietro.comin@ufpr.br>
2025-08-15 14:37:21 -0300

    [mod] bisseção finalizada juntamente com correção       de erros de outras funções.

--- ZeroFuncao.c
+++ ZeroFuncao.c
@@ -5,67 +5,99 @@
 #include "utils.h"
 #include "ZeroFuncao.h"
 
-// Função auxiliar que calcula f(x) do polinômio em questão
-real_t func (Polinomio pol, real_t x) {
-    real_t soma = 0;
-    for (int i = pol.grau; i >= 0; i--) {
-        soma += pol.p[i];
-    }
-    return soma;
+// Função auxiliar que decide qual método de calculo de polinômio usar, retornando o resultado do mesmo
+real_t calcPolinomio (Polinomio pol, real_t x, real_t *px, real_t *dpx, bool ehCalcLento) {
+
+    return ehCalcLento ? calcPolinomio_lento(pol, x, px, dpx) : calcPolinomio_rapido(pol, x, px, dpx);
 }
 
 // Retorna valor do erro quando método finalizou. Este valor depende de tipoErro
 real_t newtonRaphson (Polinomio pol, real_t x0, int criterioParada, int *it, real_t *raiz)
 {
-
+    
 }
 
 
 // Retorna valor do erro quando método finalizou. Este valor depende de tipoErro
-real_t bisseccao (Polinomio pol, real_t a, real_t b, CriterioParada wa, int *it, real_t *raiz, bool ehCalcLento)
+real_t bisseccao (Polinomio pol, real_t a, real_t b, CriterioParada cp, int *it, real_t *raiz, bool ehCalcLento)
 {
+    if (it && (*it != 0))
+        *it = 0;
+
+    if (a < b) {
+        real_t aux = a;
+        a = b;
+        b = aux;
+    }
+
     real_t xm_new, xm_old;
 
     xm_new = (a + b) / 2;
 
     real_t fa, dfa, fx, dfx;
 
-    if (ehCalcLento) {
-        calcPolinomio_lento(pol, a, &fa, &dfa);
-        calcPolinomio_lento(pol, xm_new, &fx, &dfx);
-    }
-    else {
-        calcPolinomio_lento(pol, a, &fa, &dfa);
-        calcPolinomio_lento(pol, xm_new, &fx, &dfx);
-    }
-
-    switch (wa) {
-        case CRIT1:
-
-        case CRIT2:
-
-        case CRIT3:
-            
-    }
+    calcPolinomio(pol, a, &fa, &dfa, ehCalcLento);
+    calcPolinomio(pol, xm_new, &fx, &dfx, ehCalcLento);
 
     if (fa * fx < 0) 
-        a = xm_new;
-    else if (fa * fx > 0)
         b = xm_new;
+    else if (fa * fx > 0)
+        a = xm_new;
+    else
+        return xm_new;
 
-}
+    bool critParada = false;
 
+    // Laço principal
+    while (!critParada && (*it < MAXIT)) {
 
-void calcPolinomio_rapido(Polinomio pol, real_t x, real_t *px, real_t *dpx)
-{
+        xm_old = xm_new;
+        xm_new = (a + b) / 2;
 
-}
+        calcPolinomio(pol, a, &fa, &dfa, ehCalcLento);
+        calcPolinomio(pol, xm_new, &fx, &dfx, ehCalcLento);
 
+        if (fa * fx < 0) 
+            b = xm_new;
+        else if (fa * fx > 0)
+            a = xm_new;
+        else
+            return xm_new;
 
-void calcPolinomio_lento(Polinomio pol, real_t x, real_t *px, real_t *dpx)
-{
-    for (int i = pol.grau; i >= 0; i--) {
-        *px += pol.p[i] * x;
-        *dpx += *px * i;
+        // Controla qual critério é utilizado para fim do laço
+        switch (cp) {
+            case CRIT1:
+                critParada = (fabs(xm_new - xm_old) / fabs(xm_new)) <= 10e-7;
+                break;
+            case CRIT2:
+                critParada = fabs(fx) <= DBL_EPSILON;
+                break;
+            case CRIT3:
+                critParada = ulp_distance(xm_new, xm_old) <= 3;
+                break;
+        }
+
+        *it += 1;
     }
+
+}
+
+
+real_t calcPolinomio_rapido(Polinomio pol, real_t x, real_t *px, real_t *dpx)
+{
+
+}
+
+
+real_t calcPolinomio_lento(Polinomio pol, real_t x, real_t *px, real_t *dpx)
+{
+    *px = 0.0;
+    *dpx = 0.0;
+    for (int i = pol.grau; i > 0; i--) {
+        *px += pol.p[i] * pow(x, i);
+        *dpx += i * pol.p[i] * pow(x, i - 1);
+    }
+    *px += pol.p[0];
+
+    return *px;
 }
--- utils.c
+++ utils.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
 #include "utils.h"
+#include "DoubleType.h"
 
 /*  Retorna tempo em milisegundos
 
@@ -21,3 +23,22 @@ double timestamp(void)
   return((double)(tp.tv_sec*1.0e3 + tp.tv_nsec*1.0e-6));
 }
 
+long long ulp_distance(double a, double b) {
+    // Garante que a e b não são idênticos 
+    if (a == b) {
+        return 0;
+    }
+
+    Double_t u_a, u_b;
+    u_a.f = a;
+    u_b.f = b;
+
+    if (u_a.i < 0) {
+        u_a.i = INT64_MIN - u_a.i;
+    }
+    if (u_b.i < 0) {
+        u_b.i = INT64_MIN - u_b.i;
+    }
+
+    return llabs(u_a.i - u_b.i);
+}
